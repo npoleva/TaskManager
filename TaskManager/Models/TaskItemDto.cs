130919691e40121eafcc4727038dcc62f82fdaa3
npoleva <poleva.2002@mail.ru>
2025-11-27 16:35:07 +0300

    featL add dto

---
+++ TaskManager/Models/TaskItemDto.cs
@@ -0,0 +1,10 @@
+namespace TaskManager.Models;
+
+public class TaskItemDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string? Description { get; set; }
+    public bool IsCompleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
