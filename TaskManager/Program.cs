93669f77870104fd1615ff8e250e06c27268bdbd
npoleva <poleva.2002@mail.ru>
2025-11-27 15:15:48 +0300

    feat: add db configuration

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,4 +1,17 @@
+using DotNetEnv;
+
 var builder = WebApplication.CreateBuilder(args);
+
+Env.Load("private.env");
+
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var port = Environment.GetEnvironmentVariable("DB_PORT");
+var db = Environment.GetEnvironmentVariable("DB_NAME");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString = $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
